b1279ff3a12c708550ead311225749a219538d28
Leonardo Amador <amadorleonardo8109@gmail.com>
2024-02-07 20:28:51 -0300

    Created a new activity.

--- list02/app/src/main/java/com/example/secondlist/itemDetailActivity.java
+++ list02/app/src/main/java/com/example/secondlist/itemDetailActivity.java
@@ -2,7 +2,9 @@ package com.example.secondlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class itemDetailActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class itemDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
+
+        String[] data = {"item 1", "item 2", "item 3"};
+
+        Intent receivedIntent = getIntent();
+
+        if (receivedIntent.hasExtra("clickedItemId")) {
+            int code = receivedIntent.getIntExtra("clickedItemId", 0);
+
+            TextView txvClickedItem = findViewById(R.id.txvClickedItem);
+            txvClickedItem.setText(data[code]);
+        }
+
     }
 }
