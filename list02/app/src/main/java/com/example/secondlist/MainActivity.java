0a9ce29ad4e7f2509ad627fcab1e09b1a4d5b6ad
Leonardo Amador <amadorleonardo8109@gmail.com>
2024-02-07 19:57:49 -0300

    Implemented a second list version.

---
+++ list02/app/src/main/java/com/example/secondlist/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.secondlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String[] data = {"item 1", "item 2", "item 3"};
+
+        ListView ltvList = findViewById(R.id.ltvList);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, data);
+
+        ltvList.setAdapter(adapter);
+
+        ltvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Item " + ++position + "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
